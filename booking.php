443de47fe28a8baabb2a3b60c0a2192ba538452e
Assaduzzaman Shaon <assad.uz255@gmail.com>
2025-08-26 23:47:43 +0600

    Update

--- booking.php
+++ booking.php
@@ -194,6 +194,8 @@ require_once("include/navbar.php");
 
 <!-- Bootstrap JS -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+<?php require_once("include/footer.php");
+?>
 </body>
 </html>
 <?php
